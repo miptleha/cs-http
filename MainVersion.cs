75b607e46537cb5b2eea90b1e6d14bc15bbb26dc
Aleksey Obukhov <miptleha@gmail.com>
2025-01-26 15:41:02 +0300

    add measurements

--- MainVersion.cs
+++ MainVersion.cs
@@ -1,63 +1,94 @@
 using System.Diagnostics;
+using System.Text;
 
 public class MainVersion
 {
     public static void Run()
     {
-        //TODO: Make array of description and create single loop
-        //TODO: Output results to plot them in Excel
-        Console.WriteLine($"Number of processors in system: {Environment.ProcessorCount}");
-        Console.WriteLine("Task.Run synchronous task...");
-        Executer<string, string?>.Execute_TaskRun(UrlLoader.Url, UrlLoader.Url.Count, HttpTask.Download);
-        for (int i = 1; i <= UrlLoader.Url.Count; i += 1 + (i / 10))
-        {
-            var sw = Stopwatch.StartNew();
-            var res = Executer<string, string?>.Execute_TaskRun(UrlLoader.Url, i, HttpTask.Download);
-            sw.Stop();
-            ShowInfo(i, res, sw.Elapsed);
-        }
-
-        Console.WriteLine("Task.Run asynchronous task...");
-        Executer<string, string?>.Execute_TaskRunAsync(UrlLoader.Url, UrlLoader.Url.Count, HttpTask.DownloadAsync);
-        for (int i = 1; i <= UrlLoader.Url.Count; i += 1 + (i / 10))
-        {
-            var sw = Stopwatch.StartNew();
-            var res = Executer<string, string?>.Execute_TaskRunAsync(UrlLoader.Url, i, HttpTask.DownloadAsync);
-            sw.Stop();
-            ShowInfo(i, res, sw.Elapsed);
-        }
-
-        Console.WriteLine("Wait n asynchronous task...");
-        Executer<string, string?>.Execute_BlockAsync(UrlLoader.Url, UrlLoader.Url.Count, HttpTask.DownloadAsync);
-        for (int i = 1; i <= UrlLoader.Url.Count; i += 1 + (i / 10))
-        {
-            var sw = Stopwatch.StartNew();
-            var res = Executer<string, string?>.Execute_BlockAsync(UrlLoader.Url, i, HttpTask.DownloadAsync);
-            sw.Stop();
-            ShowInfo(i, res, sw.Elapsed);
-        }
-
-        Console.WriteLine("Parallel synchronous call with PLINQ...");
-        Executer<string, string?>.Execute_PLINQ(UrlLoader.Url, UrlLoader.Url.Count, HttpTask.Download);
-        for (int i = 1; i <= UrlLoader.Url.Count; i += 1 + (i / 10))
-        {
-            var sw = Stopwatch.StartNew();
-            var res = Executer<string, string?>.Execute_PLINQ(UrlLoader.Url, i, HttpTask.Download);
-            sw.Stop();
-            ShowInfo(i, res, sw.Elapsed);
-        }
-
-        Console.WriteLine("Parallel synchronous call with Parallel loop...");
-        Executer<string, string?>.Execute_Parallel(UrlLoader.Url, UrlLoader.Url.Count, HttpTask.Download);
-        for (int i = 1; i <= UrlLoader.Url.Count; i += 1 + (i / 10))
-        {
-            var sw = Stopwatch.StartNew();
-            var res = Executer<string, string?>.Execute_Parallel(UrlLoader.Url, i, HttpTask.Download);
-            sw.Stop();
-            ShowInfo(i, res, sw.Elapsed);
-        }
-
+        Console.WriteLine($"Number of processors: {Environment.ProcessorCount}");
+        InnerRun(0, "res1");
+        InnerRun(100, "res2");
     }
+
+    static void InnerRun(int msWait, string resultFile)
+    {
+        Console.WriteLine($"Execute downloads with sleep {msWait} ms");
+        const string csvSep = ";";
+        StringBuilder csv1 = new("Name" + csvSep);
+        for (int i = 1; i <= UrlLoader.Url.Count; i++)
+            csv1.Append(i.ToString() + csvSep);
+        csv1.Append("\n");
+        foreach (var ri in _runnerInfo)
+        {
+            Console.WriteLine(ri.Description + "...");
+            csv1.Append(ri.Description + csvSep);
+            ri.Operation(UrlLoader.Url.Count, 0); //ignore first call
+            for (int i = 1; i <= UrlLoader.Url.Count; i++)
+            {
+                List<string?>? res = new();
+                //take best time
+                TimeSpan ts = new(10, 0, 0);
+                for (int j = 0; j < 2; j++)
+                {
+                    var sw = Stopwatch.StartNew();
+                    res = ri.Operation(i, msWait);
+                    sw.Stop();
+                    if (sw.Elapsed < ts)
+                        ts = sw.Elapsed;
+                }
+                ShowInfo(i, res, ts);
+                csv1.Append(Math.Round(ts.TotalSeconds, 1).ToString() + csvSep);
+            }
+            csv1.Append("\n");
+        }
+        using StreamWriter writer = new StreamWriter(resultFile + ".csv", false, Encoding.UTF8);
+        writer.Write(csv1.ToString());
+        Console.WriteLine($"File {resultFile}.csv created");
+    }
+
+    record RunnerInfo(string Description, Func<int, int, List<string?>> Operation);
+    readonly static List<RunnerInfo> _runnerInfo = [
+        new RunnerInfo(
+            "Task.Run synchronous task",
+            (i, ms) => Executer<string, string?>.Execute_TaskRun(UrlLoader.Url, i, s => 
+            { 
+                Thread.Sleep(ms);
+                return HttpTask.Download(s);
+            })
+        ),
+        new RunnerInfo(
+            "Task.Run asynchronous task",
+            (i, ms) => Executer<string, string?>.Execute_TaskRunAsync(UrlLoader.Url, i, s =>
+            {
+                Thread.Sleep(ms);
+                return HttpTask.DownloadAsync(s);
+            })
+        ),
+        new RunnerInfo(
+            "Wait for multiple asynchronous task",
+            (i, ms) => Executer<string, string?>.Execute_BlockAsync(UrlLoader.Url, i, s =>
+            {
+                Thread.Sleep(ms);
+                return HttpTask.DownloadAsync(s);
+            })
+        ),
+        new RunnerInfo(
+            "Parallel synchronous call with PLINQ",
+            (i, ms) => Executer<string, string?>.Execute_PLINQ(UrlLoader.Url, i, s =>
+            {
+                Thread.Sleep(ms);
+                return HttpTask.Download(s);
+            })
+        ),
+        new RunnerInfo(
+            "Parallel synchronous call with Parallel loop",
+            (i, ms) => Executer<string, string?>.Execute_Parallel(UrlLoader.Url, i, s =>
+            {
+                Thread.Sleep(ms);
+                return HttpTask.Download(s);
+            })
+        ),
+    ];
 
     static void ShowInfo(int n, List<string?> result, TimeSpan duration)
     {
@@ -70,5 +101,4 @@ public class MainVersion
             $"Duration: {SimpleVersion.ToStr(duration)}"
         );
     }
-
 }
--- README.md
+++ README.md
@@ -1,27 +1,34 @@
 # cs-http
-Download http resource and measure speed with different .NET approaches.  
-All links in [url.txt](url.txt) file.  
+Downloads http resources and measure speed with different .NET approaches.  
+All downloaded links are taken from [url.txt](url.txt) file.  
 For each approach try to download with arbitrary degree of parallelism.  
-After download each resource is saved in it folder to check correctness.  
-For example, `Task.Run` approach with 10 tasks for `yandex.ru` url  
-will be saved in `Task.Run\10\yandex.html` file.  
-Suffix will be added for same resources, bad requests will have empty files.  
-Finally, result of measurements will be plotted.
 
 ## HttpTask class
-[HttpTask.cs](HttpTask.cs) class loads [url.txt](url.txt) file at startup.  
-Have `synchronous` operation for download using `HttpRequest` .NET class.  
+[HttpTask.cs](HttpTask.cs) executes a get request.  
+Have `synchronous` operation for download using `HttpWebRequest` .NET class.  
 Have `asynchronous` analog for download using `HttpClient` .NET class.  
-Both operation saves result in file, perform time measure and update download statistic.
 ![task.png](task.png)  
 Operation have small cpu calculation at starting and ending and a lot of wait period for i/o task.
 
-## How to run
+## Executer
+[Executer.cs](Executer.cs) class provides different ways to run operation in parallel.  
 
-Some features are not implemented yet (plot not ready).  
-Try current application:  
+
+## Results of running
+
+Try this application:  
 ```
 dotnet run
 ```
+Outputs to console common information about measurement process.  
+Creates two `.csv` file with results for different parallel approaches and degree.  
+First file with only downloads, second one - with pause before each download to simulate some work.  
+Here is graphical view of this files:
+![res1.png](res1.png)  
+![res2.png](res2.png)
+By increasing the degree of parallelism of loading, the total processing time of all requests decreases.  
+The dependence is not linear, too many simultaneous requests do not bring any benefits.  
+The parallelization algorithm using PLINK showed the best results.
+Multiple asynchronous tasks is slower than other methods when there is a blocking wait.
 
 [![Hits](https://hits.seeyoufarm.com/api/count/incr/badge.svg?url=https%3A%2F%2Fgithub.com%2Fmiptleha%2Fcs-http&count_bg=%230C7DBD&title_bg=%23555555&icon=&icon_color=%23E7E7E7&title=hits&edge_flat=false)](https://hits.seeyoufarm.com)
